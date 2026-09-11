87520f266194c0fdc0e48ccf4ec63ea845839d32
kalainesan-n <kalainesannatarajan@gmail.com>
2026-09-12 01:14:53 +0530

    Add Week 5 fantasy assignment solutions

---
+++ src/main/java/week5/FantasyLeagueAutoDraftRankingEngine.java
@@ -0,0 +1,78 @@
+package week5;
+
+import java.util.Arrays;
+
+public class FantasyLeagueAutoDraftRankingEngine {
+
+    static class Player implements Comparable<Player> {
+
+        private String name;
+        private int matchesPlayed;
+        private double battingAverage;
+        private boolean injured;
+
+        public Player(String name, int matchesPlayed, double battingAverage, boolean injured) {
+            this.name = name;
+            this.matchesPlayed = matchesPlayed;
+            this.battingAverage = battingAverage;
+            this.injured = injured;
+        }
+
+        static boolean isDraftable(int matchesPlayed) {
+            return matchesPlayed >= 10;
+        }
+
+        static boolean isDraftable(int matchesPlayed, boolean injured) {
+            return matchesPlayed >= 5 && !injured;
+        }
+
+        public int compareTo(Player other) {
+            return Double.compare(other.battingAverage, this.battingAverage);
+        }
+    }
+
+    static String draftAndRank(Player[] players) {
+
+        Player[] draftablePlayers = new Player[players.length];
+        int count = 0;
+
+        for (int i = 0; i < players.length; i++) {
+
+            if (Player.isDraftable(players[i].matchesPlayed) ||
+                Player.isDraftable(players[i].matchesPlayed, players[i].injured)) {
+
+                draftablePlayers[count] = players[i];
+                count++;
+            }
+        }
+
+        draftablePlayers = Arrays.copyOf(draftablePlayers, count);
+
+        Arrays.sort(draftablePlayers);
+
+        String result = "";
+
+        for (int i = 0; i < draftablePlayers.length; i++) {
+
+            result = result + (i + 1) + ". " + draftablePlayers[i].name;
+
+            if (i < draftablePlayers.length - 1) {
+                result = result + " | ";
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        Player[] players = {
+            new Player("Virat", 15, 48.0, false),
+            new Player("Rahul", 7, 55.0, false),
+            new Player("Sameer", 3, 60.0, false),
+            new Player("Dev", 12, 20.0, true)
+        };
+
+        System.out.println(draftAndRank(players));
+    }
+}
